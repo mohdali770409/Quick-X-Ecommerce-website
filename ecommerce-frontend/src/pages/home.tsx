60f041accf2dc6ec7125b1fe89b1436b84fa22c3
Mohd Ali <mohdali770409@gmail.com>
2024-03-11 23:53:50 +0530

    product card completed

--- ecommerce-frontend/src/pages/home.tsx
+++ ecommerce-frontend/src/pages/home.tsx
@@ -1,8 +1,29 @@
-
+import { Link } from "react-router-dom";
+import ProductCard from "../components/product-card";
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const addToCartHandler = () => {};
 
-export default Home
+  return (
+    <div className="home">
+      <section></section>
+      <h1>
+        Latest Product
+        <Link to="/search" className="findmore">
+          find more
+        </Link>
+      </h1>
+      <main>
+        <ProductCard
+          productId="dsgsld"
+          name="Macbook"
+          price={435}
+          stock={435}
+          handler={addToCartHandler}
+          photo={"https://m.media-amazon.com/images/I/61YCWzjldDL._SX679_.jpg"}
+        />
+      </main>
+    </div>
+  );
+};
+
+export default Home;
