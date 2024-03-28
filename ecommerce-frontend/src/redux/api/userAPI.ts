57354e9e736305a92d74274ebcc7d1158fae07a1
Mohd Ali <mohdali770409@gmail.com>
2024-03-28 23:39:44 +0530

    redux setup initiated

---
+++ ecommerce-frontend/src/redux/api/userAPI.ts
@@ -0,0 +1,58 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import axios from "axios";
+import { server } from "../store";
+
+import { User } from "../../types/types";
+import {
+  AllUsersResponse,
+  DeleteUserRequest,
+  MessageResponse,
+  UserResponse,
+} from "../../types/api-types";
+
+export const userAPI = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({
+    // baseUrl: `${import.meta.env.VITE_SERVER}/api/v1/user/`,
+    baseUrl: `${server}/api/v1/user/`,
+  }),
+  tagTypes: ["users"],
+  endpoints: (builder) => ({
+    login: builder.mutation<MessageResponse, User>({
+      query: (user) => ({
+        url: "new",
+        method: "POST",
+        body: user,
+      }),
+      invalidatesTags: ["users"],
+    }),
+
+    deleteUser: builder.mutation<MessageResponse, DeleteUserRequest>({
+      query: ({ userId, adminUserId }) => ({
+        url: `${userId}?id=${adminUserId}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["users"],
+    }),
+
+    allUsers: builder.query<AllUsersResponse, string>({
+      query: (id) => `all?id=${id}`,
+      providesTags: ["users"],
+    }),
+  }),
+});
+export const getUser = async (id: string) => {
+  try {
+    const { data }: { data: UserResponse } = await axios.get(
+      `${import.meta.env.VITE_SERVER}/api/v1/user/${id}`
+    );
+
+    return data;
+  } catch (error) {
+    console.log(error);
+    // throw error;
+  }
+};
+
+export const { useLoginMutation, useAllUsersQuery, useDeleteUserMutation } =
+  userAPI;
